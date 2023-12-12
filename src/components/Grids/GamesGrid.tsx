4ccb073650628b85d611fc76c0e9c4ad9eab4af5
CleverCarpet <austinjdub@outlook.com>
2023-12-12 10:35:49 -0700

    Fix games grid

--- src/components/Grids/GamesGrid.tsx
+++ src/components/Grids/GamesGrid.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Grid, Skeleton, Container, Avatar } from '@mantine/core';
+import { SimpleGrid, Skeleton, Container, Avatar } from '@mantine/core';
 import { Card, Image, Text, Group, Badge, Button } from '@mantine/core';
 import { IconHeart } from '@tabler/icons-react';
 import classes from './BadgeCard.module.css';
@@ -8,6 +8,7 @@ const badgeList = [
     { id: 'flash', emoji: '🎮', label: 'Flash' },
     { id: 'html5', emoji: '🎮', label: 'HTML5' },
     { id: 'puzzle', emoji: '🧩', label: 'Puzzle' },
+    { id: 'story', emoji: '📖', label: 'Story' },
 ];
 
 function createBadgeList(tags: any) {
@@ -23,7 +24,7 @@ function createBadgeList(tags: any) {
 
 export function GamesGrid() {
     const [gamesList, setGamesList] = useState([]);
-    const [imageLoading, setImageLoading] = useState(true);
+    const [imageLoadingStates, setImageLoadingStates] = useState<boolean[]>([]);
     const [gamesLoading, setGamesLoading] = useState(true);
 
     useEffect(() => {
@@ -49,64 +50,64 @@ export function GamesGrid() {
 
     return (
         <Container className={classes.container} mt="md">
-            <Grid>
-                <Grid.Col span={{ base: 12, xs: 4 }}>
-                    {gamesList.map((game: any) => (
-                        <Card withBorder radius="md" p="md" className={classes.card} key={game.id}>
-                            <Card.Section>
-                                <Skeleton visible={imageLoading}>
-                                    <Image
-                                        src={`https://cdn.playvoid.xyz/data/gameContent/${game.id.slice(4)}/images/CardBanner.png`}
-                                        alt="Game Banner"
-                                        height={180}
-                                        onLoad={() => setImageLoading((l) => !l)}
-                                    />
-                                </Skeleton>
-                            </Card.Section>
+            <SimpleGrid cols={3}>
+                {gamesList.map((game: any, index: number) => (
+                    <Card withBorder radius="md" p="md" className={classes.card} key={game.id}>
+                        <Card.Section>
+                            <Skeleton visible={imageLoadingStates[index]}>
+                                <Image
+                                    src={`https://cdn.playvoid.xyz/data/gameContent/${game.id.slice(4)}/images/CardBanner.png`}
+                                    alt="Game Banner"
+                                    height={180}
+                                    onLoad={() => {
+                                        setImageLoadingStates((states) => states.map((state, i) => (i === index ? false : state)));
+                                    }}
+                                />
+                            </Skeleton>
+                        </Card.Section>
 
-                            <Card.Section className={classes.section} mt="md">
-                                <Group justify="apart">
-                                    <Avatar variant="filled" radius="sm" src={'https://cdn.playvoid.xyz/data/gameContent/' + game.id.slice(4) + '/images/PrimaryIcon.png'} />
-                                    <Text fw={600} style={{ fontSize: '25px' }}>
-                                        {game.name}
-                                    </Text>
-                                </Group>
-                                <Text fz="sm" mt="xs" className={classes.longtext}>
-                                    {game.short_description}
+                        <Card.Section className={classes.section} mt="md">
+                            <Group justify="apart">
+                                <Avatar variant="filled" radius="sm" src={'https://cdn.playvoid.xyz/data/gameContent/' + game.id.slice(4) + '/images/PrimaryIcon.png'} />
+                                <Text fw={600} style={{ fontSize: '25px' }}>
+                                    {game.name}
                                 </Text>
-                            </Card.Section>
-
-                            <Card.Section className={classes.section}>
-                                <Group gap={7} mt="md">
-                                    {createBadgeList(game.tags).map((badge: any) => (
-                                        <Badge variant="light" key={badge.label} leftSection={badge.emoji}>
-                                            {badge.label}
-                                        </Badge>
-                                    ))}
-                                </Group>
-                            </Card.Section>
-
-                            <Group mt="xs">
-                                <Button
-                                    radius="md"
-                                    style={{ flex: 1 }}
-                                    onClick={() => navigateTo(`/game/play/${game.primename}`)}
-                                >
-                                    Play Now
-                                </Button>
-                                <Button
-                                    radius="md"
-                                    variant="default"
-                                    style={{ flex: 1 }}
-                                    onClick={() => navigateTo(`/game/view/${game.primename}`)}
-                                >
-                                    View
-                                </Button>
                             </Group>
-                        </Card>
-                    ))}
-                </Grid.Col>
-            </Grid>
+                            <Text fz="sm" mt="xs" className={classes.longtext}>
+                                {game.short_description}
+                            </Text>
+                        </Card.Section>
+
+                        <Card.Section className={classes.section}>
+                            <Group gap={7} mt="md">
+                                {createBadgeList(game.tags).map((badge: any) => (
+                                    <Badge variant="light" key={badge.label} leftSection={badge.emoji}>
+                                        {badge.label}
+                                    </Badge>
+                                ))}
+                            </Group>
+                        </Card.Section>
+
+                        <Group mt="xs">
+                            <Button
+                                radius="md"
+                                style={{ flex: 1 }}
+                                onClick={() => navigateTo(`/game/play/${game.primename}`)}
+                            >
+                                Play Now
+                            </Button>
+                            <Button
+                                radius="md"
+                                variant="default"
+                                style={{ flex: 1 }}
+                                onClick={() => navigateTo(`/game/view/${game.primename}`)}
+                            >
+                                View
+                            </Button>
+                        </Group>
+                    </Card>
+                ))}
+            </SimpleGrid>
         </Container>
     );
 }
